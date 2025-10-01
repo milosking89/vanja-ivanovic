85a4ec44da7aba97269c2029ce91fe430ac1090f
milos.ivankovic <milos.ivankovic@meterandcontrol.com>
2025-10-01 06:58:08 +0200

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBga623ULNDTfLSee4rBo1mvRtmrpqEBjY",
-    authDomain: "astro-site-5f426.firebaseapp.com",
-    projectId: "astro-site-5f426",
-    storageBucket: "astro-site-5f426.firebasestorage.app",
-    messagingSenderId: "263641164662",
-    appId: "1:263641164662:web:3cbca6c215ada4de122857",
-    measurementId: "G-F2VDCYLTEB"
+    apiKey: "AIzaSyBvB9P3TTmp7d1HMzGH7lHqhtMJU_uBxqM",
+    authDomain: "vanja-db.firebaseapp.com",
+    projectId: "vanja-db",
+    storageBucket: "vanja-db.firebasestorage.app",
+    messagingSenderId: "866517019130",
+    appId: "1:866517019130:web:a503c78276c878f1789c43",
+    measurementId: "G-K9HLBGF1HY"
   }
 };
